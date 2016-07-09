44984f9ab69fd7acb53952e27a462511b5ba7cda
Rudi Theunissen <rudolf.theunissen@gmail.com>
2016-07-09 16:35:20 +1200

    Minor set.h refactor

--- src/ds/ds_set.h
+++ src/ds/ds_set.h
@@ -8,21 +8,19 @@
 #define DS_SET_CAPACITY(s) ((s)->table->capacity)
 #define SET_IS_EMPTY(s)    (DS_SET_SIZE(s) == 0)
 
-#define DS_SET_FOREACH(set, value) \
-    DS_HTABLE_FOREACH_KEY(set->table, value)
-
-#define DS_SET_FOREACH_END() \
-    DS_HTABLE_FOREACH_END()
+#define DS_SET_FOREACH(set, value)  DS_HTABLE_FOREACH_KEY(set->table, value)
+#define DS_SET_FOREACH_END()        DS_HTABLE_FOREACH_END()
 
 typedef struct _ds_set_t {
     ds_htable_t *table;
 } ds_set_t;
 
-ds_set_t *ds_set_ex(ds_htable_t *table);
 ds_set_t *ds_set();
-void      ds_set_free(ds_set_t *set);
+ds_set_t *ds_set_ex(ds_htable_t *table);
 ds_set_t *ds_set_clone(ds_set_t *set);
 
+void ds_set_free(ds_set_t *set);
+void ds_set_clear(ds_set_t *set);
 void ds_set_allocate(ds_set_t *set, zend_long capacity);
 
 void ds_set_add(ds_set_t *set, zval *value);
@@ -30,7 +28,7 @@ void ds_set_add_va(ds_set_t *set, VA_PARAMS);
 bool ds_set_contains_all(ds_set_t *set, VA_PARAMS);
 bool ds_set_contains(ds_set_t *set, zval *value);
 void ds_set_remove_va(ds_set_t *set, VA_PARAMS);
-void ds_set_clear(ds_set_t *set);
+
 void ds_set_to_array(ds_set_t *set, zval *arr);
 void ds_set_add_all(ds_set_t *set, zval *value);
 
@@ -40,18 +38,19 @@ zval *ds_set_get_last(ds_set_t *set);
 
 ds_set_t *ds_set_slice(ds_set_t *set, zend_long index, zend_long length);
 
-void      ds_set_sort_callback(ds_set_t *set);
-void      ds_set_sort(ds_set_t *set);
+void ds_set_sort_callback(ds_set_t *set);
+void ds_set_sort(ds_set_t *set);
+
 ds_set_t *ds_set_sorted_callback(ds_set_t *set);
 ds_set_t *ds_set_sorted(ds_set_t *set);
 
-void ds_set_join  (ds_set_t *set, const char *glue, const size_t len, zval *return_value);
+void ds_set_join (ds_set_t *set, const char *glue, const size_t len, zval *return_value);
 void ds_set_reduce(ds_set_t *set, FCI_PARAMS, zval *initial, zval *return_value);
 
 ds_set_t *ds_set_filter_callback(ds_set_t *set, FCI_PARAMS);
 ds_set_t *ds_set_filter(ds_set_t *set);
 
-void      ds_set_reverse (ds_set_t *set);
+void ds_set_reverse (ds_set_t *set);
 ds_set_t *ds_set_reversed(ds_set_t *set);
 
 ds_set_t *ds_set_diff(ds_set_t *set, ds_set_t *other);
