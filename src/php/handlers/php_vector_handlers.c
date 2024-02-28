53b57937dd227906e2b26c55248512110fbc8902
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2024-02-28 17:17:46 +0100

    Fix #206: Null coalescing operator on Vector make search failed
    This happens because php_ds_vector_read_dimension makes a REF from the value
    when type is not BP_VAR_R. So when the type is BP_VAR_IS then we see the first
    element becomes a reference. You can see this when doing var_dump.

--- src/php/handlers/php_vector_handlers.c
+++ src/php/handlers/php_vector_handlers.c
@@ -37,7 +37,7 @@ static zval *php_ds_vector_read_dimension
 
     // If we're accessing by reference we have to create a reference.
     // This is for access like $deque[$a][$b] = $c
-    if (value && type != BP_VAR_R) {
+    if (value && type != BP_VAR_R && type != BP_VAR_IS) {
         ZVAL_MAKE_REF(value);
     }
 
